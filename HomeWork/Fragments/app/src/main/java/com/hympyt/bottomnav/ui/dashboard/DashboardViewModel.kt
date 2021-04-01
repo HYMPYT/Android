3f79a22a63eace16fd1cb905611a1c1e96049348
HYMPYT <vgenlevchuk2016@gmail.com>
2021-04-01 22:21:36 +0300

    Uploaded homework about BottomNavigationView

---
+++ HomeWork/Fragments/app/src/main/java/com/hympyt/bottomnav/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,12 @@
+package com.hympyt.bottomnav.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>()
+    val text: LiveData<String>
+            get() = _text
+}
---
+++ HomeWork/Fragments/app/src/main/java/com/hympyt/bottomnav/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,12 @@
+package com.hympyt.bottomnav.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>()
+    val text: LiveData<String>
+            get() = _text
+}
